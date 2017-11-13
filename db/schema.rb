032d8b6d9237873b8d8e2ac95f0b4da5b27440e9
Sasao Tomohiro <sasao.t.aa@m.titech.ac.jp>
2017-11-13 05:56:41 +0000

    Add postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111090533) do
+ActiveRecord::Schema.define(version: 20171113031213) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171111090533) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "age"
   end
 
 end
